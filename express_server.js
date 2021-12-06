f73f4474794b8e8b85165dec7a81860c8e968d21
Mark Chuang <markchuangth@gmail.com>
2021-12-06 17:55:36 -0500

    the server is now set up, listening on port 8080

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
