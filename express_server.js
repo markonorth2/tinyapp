36f96b742cc1668cf5704eec81d02580700167e9
Mark Chuang <markchuangth@gmail.com>
2021-12-06 23:24:28 -0500

    :shortURL works now.

--- express_server.js
+++ express_server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+//Set the view engine to ejs
+app.set("view engine", 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,3 +17,23 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shorturl_name = req.params.shortURL
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shorturl_name]};
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
